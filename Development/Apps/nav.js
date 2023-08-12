e39bb8b218ed1f61d1b5c1ba910760e43c459e99
RadiantPrism <tonio.barbulescu@gmail.com>
2023-08-12 22:40:17 +0100

    Improving dynamic nav
    Just need to work with the placement of the nav, and animations.

--- Development/Apps/nav.js
+++ Development/Apps/nav.js
@@ -1,12 +1,19 @@
 const nav = document.querySelector("nav");
+const nav_ul = nav.querySelector("ul");
 
 window.addEventListener("scroll", (scrollEvent) => {
   let scrollAmount = window.scrollY;
 
   if(scrollAmount > 0) {
-    nav.style.position = "fixed";
+    nav_ul.style.justifyContent = "flex-end";
+    nav_ul.style.display = "block";
+    nav_ul.style.top = "50%";
+    nav_ul.style.transform = "translateY(-50%)";
     return;
   }
 
-  nav.style.position = "relative";
+  nav_ul.style.justifyContent = "center";
+  nav_ul.style.display = "flex";
+  nav_ul.style.top = "0";
+  nav_ul.style.transform = "translateY(0)";
 });
